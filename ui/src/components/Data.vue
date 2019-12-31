1931891d417508a028152b1a55ac88412377985a
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-12-30 16:20:55 -0800

    material adds

--- ui/src/components/Data.vue
+++ ui/src/components/Data.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container">
     <div class="col">
-      <p>{{ sorted_bitmex_bids }}</p>
+      <p>{{ sorted_bitmex_bids }} {{ sorted_bitmex_asks }}</p>
     </div>
     <div class="col">
-      <p>{{ sorted_bitmex_asks }}</p>
-    </div>
-    <div class="col">
-      <!-- <p>{{ data }}</p> -->
+      <p>{{ sorted_binance_bids }} {{ sorted_binance_asks }}</p>
     </div>
   </div>
 </template>
@@ -19,13 +16,15 @@ export default {
   name: 'Data',
   computed: {
     sorted_binance_asks: function() {
-      return
+      let items = this.binance_asks.sort((a, b) => { return a - b })
+      return items.slice(0,5)
     },
 
     sorted_binance_bids: function() {
-      return
+      let items = this.binance_asks.sort((a, b) => { return b -  a})
+      return items.slice(0,5)
     },
-    
+  
     sorted_bitmex_asks: function() {
       let items = this.bitmex_asks.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
       return items.slice(0,5)
@@ -49,16 +48,44 @@ export default {
     try {
       let ws = new WebSocket("ws://localhost:8000/ws")
       ws.onmessage = ((e) => {
-        switch (JSON.parse(e.data)["host"]) {
+        let dump = JSON.parse(e.data)
+
+        switch (dump["host"]) {
           case "binance":
-            this.binance_asks = e.data["asks"]
-            this.binance_bids = e.data["bids"]
+            this.binance_asks = dump["asks"]
+            this.binance_bids = dump["bids"]
             break
 
           case "bitmex":
-            switch (e.data["action"]) {
+            switch (dump["action"]) {
               case "update":
-                
+                for (let i = 0; i < dump["data"].length; i++) {
+                  if (dump["data"][i]["side"] == "Buy") {
+                    this.bitmex_bids[this.bitmex_bids.findIndex(n => n.id === dump["data"][i]["id"])]["size"] = dump["data"][i]["size"]
+                  } else if (dump["data"][i]["side"] == "Sell") {
+                    this.bitmex_asks[this.bitmex_asks.findIndex(n => n.id === dump["data"][i]["id"])]["size"] = dump["data"][i]["size"]
+                  }
+                }
+                break
+
+              case "delete":
+                for (let i = 0; i < dump["data"].length; i++) {
+                  if (dump["data"][i]["side"] == "Buy") {
+                    this.bitmex_bids.splice(this.bitmex_bids.findIndex(n => n.id === dump["data"][i]["id"]), 1)
+                  } else if (dump["data"][i]["side"] == "Sell") {
+                    this.bitmex_asks.splice(this.bitmex_asks.findIndex(n => n.id === dump["data"][i]["id"]), 1)
+                  }
+                }
+                break
+
+              case "insert":
+                for (let i = 0; i < dump["data"].length; i++) {
+                  if (dump["data"][i]["side"] == "Buy") {
+                    this.bitmex_bids.push(dump["data"][i])
+                  } else if (dump["data"][i]["side"] == "Sell") {
+                    this.bitmex_asks.push(dump["data"][i])
+                  }
+                }
                 break
 
               default:
@@ -68,8 +95,8 @@ export default {
             break
 
           case "okex":
-            this.okex_asks = e.data["data"]["asks"]
-            this.okex_bids = e.data["data"]["bids"]
+            this.okex_asks = dump["data"]["asks"]
+            this.okex_bids = dump["data"]["bids"]
             break
           
           default:
