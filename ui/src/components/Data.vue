c6ad036ecea402c1c95bb837ef7ab16ca0a0e68e
Matt Gorzka <mgorzk@gmail.com>
2019-12-31 23:37:34 -0800

    vue devs

--- ui/src/components/Data.vue
+++ ui/src/components/Data.vue
@@ -1,9 +1,58 @@
 <template>
-  <div class="container">
-    <div class="col"
+  <div class="leveltwo">
+    <div 
+      class="col"
       v-cloak>
-      <p>{{ asksBitmex }}</p>
-      <p>{{ bidsBitmex }}</p>
+        <div
+        v-for="l in levelTwoBids" 
+        :key="l">
+        <span class="exchange">{{ l.exchange }}</span>
+        </div>
+    </div>
+    <div 
+      class="col"
+      v-cloak>
+        <div
+        v-for="l in levelTwoBids" 
+        :key="l">
+        <span class="price">{{ l.price }}</span>
+        </div>
+    </div>
+    <div 
+      class="col"
+      v-cloak>
+        <div
+        v-for="l in levelTwoBids" 
+        :key="l">
+        <span class="size">{{ parseFloat(l.size).toFixed(3) }}</span>
+        </div>
+    </div> 
+    <div 
+      class="col"
+      v-cloak>
+        <div
+        v-for="l in levelTwoAsks" 
+        :key="l">
+        <span class="exchange">{{ l.exchange }}</span>
+        </div>
+    </div>
+    <div 
+      class="col"
+      v-cloak>
+        <div
+        v-for="l in levelTwoAsks" 
+        :key="l">
+        <span class="price">{{ l.price }}</span>
+        </div>
+    </div>
+    <div 
+      class="col"
+      v-cloak>
+        <div
+        v-for="l in levelTwoAsks" 
+        :key="l">
+        <span class="size">{{ parseFloat(l.size).toFixed(3) }}</span>
+        </div>
     </div>
   </div>
 </template>
@@ -12,10 +61,6 @@
 import axios from 'axios'
 
 export default {
-  computed: {
-    
-  },
-
   created() {
     try {
       let ws = new WebSocket("ws://localhost:8000/ws")
@@ -26,24 +71,15 @@ export default {
           case "binance":
             this.asksBinance = dump["asks"]
               .map((m) => { return {"exchange": "BNC", "price": m[0], "size": m[1]} })
-                .sort((a, b) => { return a.price - b.price })
 
             this.bidsBinance = dump["bids"]
               .map((m) => { return {"exchange": "BNC", "price": m[0], "size": m[1]} })
-                .sort((a, b) => { return b.price - a.price })
 
             break
 
           case "bitmex":
             switch (dump["action"]) {
               case "update":
-                let bmxBuy = dump["data"]
-                  .filter(b => b.side === "Buy")
-                    .map((m) => {
-                      let n = this.bidsBitmex.findIndex(o => m.id === o.id)
-                      this.bidsBitmex[n].size = m.size
-                  })
-
                 let bmxSell = dump["data"]
                   .filter(a => a.side === "Sell")
                     .map((m) => {
@@ -51,17 +87,45 @@ export default {
                       this.asksBitmex[n].size = m.size
                   })
 
+                let bmxBuy = dump["data"]
+                  .filter(b => b.side === "Buy")
+                    .map((m) => {
+                      let n = this.bidsBitmex.findIndex(o => m.id === o.id)
+                      this.bidsBitmex[n].size = m.size
+                  })
+                
                 break
 
               case "delete":
-                
-                
+                dump["data"]
+                  .filter(a => a.side === "Sell")
+                    .map((m) => {
+                      let n = this.asksBitmex.findIndex(o => m.id === o.id)
+                      this.asksBitmex.splice(n, 1)
+                    })
+
+                dump["data"]
+                  .filter(b => b.side === "Bids")
+                    .map((m) => {
+                      let n = this.bidsBitmex.findIndex(o => m.id === o.id)
+                      this.bidsBitmex.splice(n, 1)
+                    })
+
                 break
 
               case "insert":
-                // for (let i = 0; i < dump["data"].length; i++) {
+                dump["data"]
+                  .filter(a => a.side === "Sell")
+                    .map((m) => {
+                      this.asksBinance.push({"exchange": "BMX", "id": m.id, "price": m.price, "size": m.size})
+                  })
+
+                dump["data"]
+                  .filter(b => b.side === "Buy")
+                    .map((m) => {
+                      this.bidsBinance.push({"exchange": "BMX", "id": m.id, "price": m.price, "size": m.size})
+                  })
                   
-                // }
                 break
 
               default:
@@ -73,11 +137,9 @@ export default {
           case "okex":
             this.asksOkex = dump["data"][0]["asks"]
               .map((m) => { return {"exchange": "OKX", "price": m[0], "size": m[1]} })
-                .sort((a, b) => { return a.price - b.price })
 
             this.bidsOkex = dump["data"][0]["bids"]
               .map((m) => { return {"exchange": "OKX", "price": m[0], "size": m[1]} })
-                .sort((a, b) => { return b.price - a.price })
 
             break
           
@@ -85,7 +147,16 @@ export default {
             break
         }
 
-        
+      let asks = this.asksOkex.concat(this.asksBinance, this.asksBitmex)
+      this.levelTwoAsks = asks.sort((a, b) => {
+        return a.price - b.price
+      }).slice(0, 15)
+
+      let bids = this.bidsOkex.concat(this.bidsBinance, this.bidsBitmex)
+      this.levelTwoBids = bids.sort((a, b) => {
+        return b.price - a.price
+      }).slice(0, 15)
+
       })
     } catch (e) {
       console.log(e)
@@ -99,7 +170,9 @@ export default {
       asksOkex: [],
       bidsBinance: [],
       bidsBitmex: [],
-      bidsOkex: []
+      bidsOkex: [],
+      levelTwoAsks: [],
+      levelTwoBids: [],
     }
   },
 
@@ -133,19 +206,28 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Zeyada&display=swap');
 
-body {
+.col {
   display: inline-block;
-  margin: 0;
-  font-weight: 200;
-  font-size: 16px;
+  margin: 0px;
+  width: 9%;
 }
 
-#content {
+.exchange {
+  font-weight:200;
+  font-size:14px;
+}
+
+.leveltwo {
+  display: inline-block;
+  margin: 0px;
+  text-align: center;
   padding-top: 3%;
+  width: 75%;
 }
 
 @keyframes appear {
   from { opacity: 0; }
   to { opacity: 1; }
 }
+
 </style>
